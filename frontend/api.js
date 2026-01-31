de58d9e83c21f2c3ffa03f1b5da4bd6070709366
alxo66 <alxo6677@gmail.com>
2026-01-31 23:52:15 +0500

    api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,12 +1,6 @@
-export async function createOrder(data) {
-  await fetch("http://localhost:3000/api/order", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  });
-}
+const API_URL = "https://<applestore-backend-production.up.railway.app>";
 
-export async function getRates() {
-  const res = await fetch("http://localhost:3000/api/rates");
+export async function getDepositData() {
+  const res = await fetch(`${API_URL}/api/deposit`);
   return await res.json();
 }
