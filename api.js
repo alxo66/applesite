969deda751a37e77d186db631dd3b69af67516d0
alxo66 <alxo6677@gmail.com>
2026-01-31 23:53:18 +0500

    Delete api.js

--- api.js
+++
@@ -1,6 +0,0 @@
-const API_URL = "https://<applestore-backend-production.up.railway.app>";
-
-export async function getDepositData() {
-  const res = await fetch(`${API_URL}/api/deposit`);
-  return await res.json();
-}
