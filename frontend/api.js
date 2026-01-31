7a366bc30fd033b21ca56b66cbbbdf36d59549f2
alxo66 <alxo6677@gmail.com>
2026-02-01 01:32:32 +0500

    api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,21 +1,25 @@
 const API_BASE = "https://applestore-backend-production.up.railway.app/api";
 
-async function apiFetch(path, options = {}) {
-  try {
-    const res = await fetch(API_BASE + path, {
-      headers: {
-        "Content-Type": "application/json"
-      },
-      ...options
-    });
-
-    if (!res.ok) {
-      throw new Error("API error: " + res.status);
-    }
-
-    return await res.json();
-  } catch (err) {
-    console.error("API FETCH ERROR:", err);
-    throw err;
+function getUserId() {
+  let id = localStorage.getItem("userId");
+  if (!id) {
+    id = crypto.randomUUID();
+    localStorage.setItem("userId", id);
   }
+  return id;
+}
+
+async function api(path, options = {}) {
+  const userId = getUserId();
+
+  const res = await fetch(API_BASE + path, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      "x-user-id": userId,
+      ...(options.headers || {})
+    }
+  });
+
+  return res.json();
 }
