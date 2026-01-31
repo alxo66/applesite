14897075e88c39d8e7b9379281bd7dff6b760681
alxo66 <alxo6677@gmail.com>
2026-01-31 20:24:26 +0500

    config.js

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const TELEGRAM_TOKEN = "8363922433:AAFBJ_g338SwrvAN77y90mLqi3OjuiEFnM8";
+export const TELEGRAM_CHAT_ID = "@crypto_applestore_bot";
+
+export const WALLETS = {
+  BTC: "bc1qlgf034j5nhqh0ltsqnhrepchlxwlykrtujvupq",
+  ETH: "0x5Fc25f19E18Dfc7d19595cB7d1eB0D0605b9A3FA",
+  USDT: "TMM1xGXxAY9R66hGPxKNfxo81KrmdyrszE",
+  TON: "UQD-XSYf6P-NyjbSJYDHsgHnk0e5CiJQ2-NCZddro_5-c8B4"
+};
